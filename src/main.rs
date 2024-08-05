7c8ab99f3349ebc3a13d3df8cefa5d4bdb5811ce
ralphvw <ralphvwilliams@icloud.com>
2024-08-05 12:54:27 +0000

    init

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+use std::collections::HashMap;
+
+fn main() {
+    let mut args = std::env::args().skip(1);
+    let key = args.next().expect("Please enter a key");
+    let value = args.next().expect("Please enter a value");
+
+    let contents = format!("{}\t{}\n", key, value);
+    std::fs::write("kv.db", contents).unwrap();
+}
+
+struct Database {
+    map: HashMap<String, String>,
+}
+
+impl Database {
+    fn new() -> Result<Database, std::io::Error> {
+        let contents = std::fs::read_to_string("kv.db")?;
+
+        let mut map = HashMap::new();
+        for line in contents.lines() {
+            let (key, value) = line.split_once('\t').expect("Corrupt database");
+            map.insert(key.to_owned(), value.to_owned());
+        }
+        Ok(Database { map })
+    }
+}
